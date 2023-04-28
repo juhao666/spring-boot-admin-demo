1d2325596cf33c17cb44a29fabc53fa9d46175af
Erik Liu <liu_jianying1985@hotmail.com>
2023-04-28 08:11:17 +0800

    first commit

---
+++ src/test/java/com/juhao666/monitor/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhao666.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
